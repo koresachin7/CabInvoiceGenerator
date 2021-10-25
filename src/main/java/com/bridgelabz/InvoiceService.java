2d01bc9d51545b8502082e8e9f6c0dfeb18b0c4e
koresachin7 <koresachin7@gmail.com>
2021-10-26 00:05:47 +0530

    Added : - To Invoice generator should now return

--- src/main/java/com/bridgelabz/InvoiceService.java
+++ src/main/java/com/bridgelabz/InvoiceService.java
@@ -19,11 +19,19 @@ public class InvoiceService {
         return totalFare;
     }
 
-    public double calculateFare(Ride[] rides) {
+    public double calculateTotalFare(Ride[] rides) {
         double totalFare = 0.0;
         for (Ride ride : rides) {
             totalFare = calculateFare(ride.getDistance(), ride.getTime());
         }
         return totalFare;
+
+    }
+    public InvoiceSummary calculateFare(Ride[] rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare += calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return new InvoiceSummary(rides.length, totalFare);
     }
 }
