4b15c257f16be352fffdece877b663b30d8b05cc
koresachin7 <koresachin7@gmail.com>
2021-10-25 23:22:41 +0530

    Added : - Given distance and time, Invoice generator

---
+++ src/main/java/com/bridgelabz/InvoiceService.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+/**
+ * Purpose  - Given distance and time, Invoice generator should return the total fare of the journey
+ * @author  - Sachin Kore
+ * @version - 16.0
+ */
+
+public class InvoiceService {
+    private static final double COST_PER_KM = 10;
+    private static final double COST_PER_MIN = 1;
+    private static final double MIN_FARE = 5.0;
+
+    // Created a method to calculate the fare of journey
+    public double CalculateFare(double distance, int time) {
+        double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+        if (totalFare < MIN_FARE) {
+            return MIN_FARE;
+        }
+        return totalFare;
+    }
+}
